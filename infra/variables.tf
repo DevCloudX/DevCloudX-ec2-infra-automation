83b49183d1cd2e08e6b3c1ce0a85bdddb9831eb9
Abhishek-X-Rana_qd <Abhishek.X.Rana@questdiagnostics.com>
2025-05-08 10:26:05 +0000

    added

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,46 @@
-variable "region" {}
-variable "vpc_name" {}
-variable "vpc_cidr" {}
-variable "subnet_name" {}
-variable "subnet_cidr" {}
-variable "key_pair_name" {}
-variable "instance_type" {}
-variable "instance_name" {}
-variable "volume_size" {}
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone for subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "volume_size" {
+  description = "Root volume size in GB"
+  type        = number
+  default     = 8
+}

8a054e6a8025dc3827996d2fb1d5925351adf44b
Abhishek-X-Rana_qd <Abhishek.X.Rana@questdiagnostics.com>
2025-05-09 12:11:00 +0000

    added

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
 
 resource "aws_vpc" "main" {
@@ -11,10 +11,14 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_cidr
-  availability_zone       = var.aws_az
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.aws_az
   map_public_ip_on_launch = true
 
   tags = {
@@ -22,35 +26,24 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.vpc_name}-igw"
-  }
-}
-
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "${var.vpc_name}-public-rt"
-  }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_security_group" "ec2_sg" {
+resource "aws_security_group" "sg" {
   name        = var.security_group_name
-  description = "Allow SSH and ICMP"
+  description = "Allow SSH and HTTP"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -61,9 +54,9 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -80,18 +73,15 @@ resource "aws_security_group" "ec2_sg" {
 }
 
 resource "aws_instance" "ec2" {
-  ami                    = var.ec2_ami_id
-  instance_type          = var.ec2_instance_type
-  subnet_id              = aws_subnet.public.id
-  key_name               = var.key_pair_name
+  count         = var.vm_count
+  ami           = var.ec2_ami_id
+  instance_type = var.ec2_instance_type
+  subnet_id     = aws_subnet.main.id
+  key_name      = var.key_pair_name
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = var.ec2_name
+    Name = "${var.ec2_name}_server${format("%02d", count.index + 1)}"
   }
-}
-
-output "public_ip" {
-  value = aws_instance.ec2.public_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,6 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-variable "vpc_name" {}
-variable "vpc_cidr" {}
-variable "subnet_name" {}
-variable "subnet_cidr" {}
-variable "security_group_name" {}
-variable "ec2_name" {}
-variable "key_pair_name" {}
-
 variable "aws_region" {
   default = "us-east-1"
 }
@@ -22,5 +14,16 @@ variable "ec2_instance_type" {
 }
 
 variable "ec2_ami_id" {
-  default = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (HVM), SSD Volume Type in us-east-1
+  default = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+}
+
+variable "vpc_name" {}
+variable "vpc_cidr" {}
+variable "subnet_name" {}
+variable "subnet_cidr" {}
+variable "security_group_name" {}
+variable "key_pair_name" {}
+variable "ec2_name" {}
+variable "vm_count" {
+  type = number
 }
